2bc3a6911dff33e774e1a631c022a66fa071435e
jinnyjang <jinnjintanavasan@gmaill.com>
2013-08-29 00:49:22 +0700

    Revert "A Update 29/08/2013 v01"
    This reverts commit 4bb75f19d47407ac21670d76e8dc4ba7f0311b1c.

--- application/views/homeView.php
+++ application/views/homeView.php
@@ -4,19 +4,10 @@
     <head>
         
     </head>
-    <style>
-        .container {
-         background: #f7f7f7;
-         background-repeat:no-repeat;
-         background-position:right top;
-        /*margin-right:200px;*/
-        }
-    </style>
+    
     <body>
-        <div>
-            Site Url: <?php echo site_url('home/x')?><br/>
-            Base Url: <?php echo base_url('home/x')?>
-        </div>
+        Site Url: <?php echo site_url('home/x')?><br/>
+        Base Url: <?php echo base_url('home/x')?>
     </body>
     
 </html>
